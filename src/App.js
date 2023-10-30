fd52e3894b775ab83cc2b57e6258010f55b20cdd
ABDUR-RAZZAK-ROUZEX <abdurrazzak1663@gmail.com>
2023-10-30 17:22:57 +0600

    general idea of using react-query is developed and respective codes are commented here for future reference

--- src/App.js
+++ src/App.js
@@ -3,31 +3,37 @@ import "./App.css";
 import SuperHeroesPage from "./components/SuperHeroes.page";
 import HomePage from "./components/Home.page";
 import RQSuperHeroesPage from "./components/RQSuperHeroes.page";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/super-heroes" element={<SuperHeroesPage />} />
-          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/super-heroes" element={<SuperHeroesPage />} />
+            <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          </Routes>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 
--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,7 +1,55 @@
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
+
+const getSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 
 const RQSuperHeroesPage = () => {
-  return <div>RQSuperHeroesPage</div>;
+  const { isLoading, data, isError, error } = useQuery(
+    "super-heroes",
+    getSuperHeroes,
+    {
+      // staleTime: 30000
+      refetchOnWindowFocus: false,
+      //   refetchInterval: 2000
+      //   onSuccess: onSuccessMethod,
+      //   onError: onErrorMethod,
+      /*       select: (data) => {
+        // Here you can modify the data you received from the API call
+        const superHeroesNames = data?.data?.map((hero) => hero.name);
+        return superHeroesNames;
+      }, */
+    }
+  );
+
+  // for calling an API based on user events eg. click
+  /*   const { isLoading, data, isError, error, refetch } = useQuery(
+    "super-heroes",
+    getSuperHeroes,
+    {
+        enabled: false
+    }
+  ); */
+
+  if (isLoading) {
+    return <h2>Loading....</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error?.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super heroes page</h2>
+      {/* <button onClick={refetch}>Click to fetch data</button> */}
+      {data?.data?.map((hero) => {
+        return <div key={hero?.name}>{hero?.name}</div>;
+      })}
+    </>
+  );
 };
 
 export default RQSuperHeroesPage;
