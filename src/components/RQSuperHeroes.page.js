ac7ad7211ec1b280267556caa1b8fe5f2b6ab135
ABDUR-RAZZAK-ROUZEX <abdurrazzak1663@gmail.com>
2023-10-31 16:07:40 +0600

    prefetching example is added

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+import { useQuery, useQueryClient } from "react-query";
+import { getSuperHero } from "../hooks/useSuperHeroData";
 
 const RQSuperHeroesPage = () => {
+  const queryClient = useQueryClient();
   const { isLoading, data, isError, error } = useSuperHeroesData();
 
   // for calling an API based on user events eg. click
@@ -13,6 +16,12 @@ const RQSuperHeroesPage = () => {
         enabled: false
     }
   ); */
+
+  const prefetchData = () => {
+    queryClient.prefetchQuery(["super-heroes", 1], getSuperHero, {
+      staleTime: 5000,
+    });
+  };
 
   if (isLoading) {
     return <h2>Loading....</h2>;
@@ -29,7 +38,12 @@ const RQSuperHeroesPage = () => {
       {data?.data?.map((hero) => {
         return (
           <div key={hero?.id}>
-            <Link to={`/rq-super-heroes/${hero?.id}`}>{hero?.name}</Link>
+            <Link
+              to={`/rq-super-heroes/${hero?.id}`}
+              onMouseEnter={prefetchData}
+            >
+              {hero?.name}
+            </Link>
           </div>
         );
       })}
--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useQuery, useQueryClient } from "react-query";
 
-const getSuperHero = ({ queryKey }) => {
+export const getSuperHero = ({ queryKey }) => {
   const heroId = queryKey[1];
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
