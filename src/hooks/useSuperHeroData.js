3ffb306ef3ef42e4d3f49b2557e12866edb06625
ABDUR-RAZZAK-ROUZEX <abdurrazzak1663@gmail.com>
2023-10-31 12:08:58 +0600

    initial query data example is added

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,12 +1,27 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 const getSuperHero = ({ queryKey }) => {
-  console.log("query key: ", queryKey);
   const heroId = queryKey[1];
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
 
 export const useSuperHeroData = (heroId) => {
   return useQuery(["super-heroes", heroId], getSuperHero);
+
+  /* for setting the initial Query Data */
+  const queryClient = useQueryClient();
+  return useQuery(["super-heroes", heroId], getSuperHero, {
+    initialData: () => {
+      const hero = queryClient
+        .getQueriesData("super-heroes")
+        ?.data?.find((hero) => hero.id == parseInt(heroId));
+
+      if (hero) {
+        return { data: hero };
+      } else {
+        return undefined;
+      }
+    },
+  });
 };
